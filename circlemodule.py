655e122528e8db2065ca9201e6c48153c828015a
manan36chauhan <63107421+manan36chauhan@users.noreply.github.com>
2022-06-09 13:50:22 +0530

    Initial commit

---
+++ circlemodule.py
@@ -0,0 +1,4 @@
+def area(r):
+    return r*r*3.14
+def circumference(r):
+    return 2*r*3.14
---
+++ import.py
@@ -0,0 +1,7 @@
+import circlemodule as g
+r=int(input("enter radius"))
+a=g.area(r)
+d=g.circumference(r)
+print("radius=",r)
+print("area=",a)
+print("circumference=",d)
